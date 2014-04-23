fce70af33788b9b0434572b3f004d1dc6eca3a03
Jack Farrelly <jack_farrelly@yahoo.co.uk>
2014-04-23 17:55:17 +0100

    added g hash map

--- config.c
+++ config.c
@@ -1,11 +1,14 @@
 #include "config.h"
+#include <stdbool.h>
+#include <glib.h>
+
+GHashTable *config;
 
 void parse(void)
 {
+    config = g_hash_table_new(g_str_hash, g_str_equal);
     yaml_parser_t parser;
     yaml_event_t event;
-
-    int done = 0;
 
     /* Create the Parser object. */
     yaml_parser_initialize(&parser);
@@ -15,27 +18,31 @@ void parse(void)
 
     yaml_parser_set_input_file(&parser, input);
 
-    /* Read the event sequence. */
-    while (!done) {
-
-        /* Get the next event. */
-        if (!yaml_parser_parse(&parser, &event))
+    do {
+        if (!yaml_parser_parse(&parser, &event)) {
+            printf("Parser error %d\n", parser.error);
             goto end;
+        }
 
-        /*
-          ...
-          Process the event.
-          ...
-        */
-
-        /* Are we finished? */
-        done = (event.type == YAML_STREAM_END_EVENT);
-
-        /* The application is responsible for destroying the event object. */
-        yaml_event_delete(&event);
-
-    }
+        switch(event.type)
+        { 
+            case YAML_NO_EVENT: puts("No event!"); break;
+            /* Stream start/end */
+            case YAML_STREAM_START_EVENT: puts("STREAM START"); break;
+            case YAML_STREAM_END_EVENT:   puts("STREAM END");   break;
+            /* Block delimeters */
+            case YAML_DOCUMENT_START_EVENT: puts("<b>Start Document</b>"); break;
+            case YAML_DOCUMENT_END_EVENT:   puts("<b>End Document</b>");   break;
+            case YAML_SEQUENCE_START_EVENT: puts("<b>Start Sequence</b>"); break;
+            case YAML_SEQUENCE_END_EVENT:   puts("<b>End Sequence</b>");   break;
+            case YAML_MAPPING_START_EVENT:  puts("<b>Start Mapping</b>");  break;
+            case YAML_MAPPING_END_EVENT:    puts("<b>End Mapping</b>");    break;
+            /* Data */
+            case YAML_ALIAS_EVENT:  printf("Got alias (anchor %s)\n", event.data.alias.anchor); break;
+            case YAML_SCALAR_EVENT: printf("Got scalar (value %s)\n", event.data.scalar.value); break;
+        }
+    } while(event.type != YAML_STREAM_END_EVENT);
 
     end:
-        yaml_parser_delete(&parser);
+        yaml_event_delete(&event);
 }
